2937ab090c7fd37ed8a8ed91de8416b3bbf05001
React330 <73373287+React330@users.noreply.github.com>
2022-05-02 14:09:09 +0900

    ReactJS-learning router Add 404 error rpages

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
   return (
     <div>
       <h1>UrlParameterの画面です</h1>
       <p>パラメータのidは {id} です</p>
+      <p>クエリパラメータのidは {query.get("name")} です</p>
     </div>
   );
 };
